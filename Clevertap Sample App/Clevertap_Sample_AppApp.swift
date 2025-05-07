d62f92b2a67bb19609602539753686b5b01c30c8
Govind Pathak <govind@cavacham.com>
2025-05-07 15:55:15 +0530

    Initial Commit

---
+++ Clevertap Sample App/Clevertap_Sample_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clevertap_Sample_AppApp.swift
+//  Clevertap Sample App
+//
+//  Created by Govind Pathak on 07/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Clevertap_Sample_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
